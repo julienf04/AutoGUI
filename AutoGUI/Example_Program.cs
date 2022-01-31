87fc861421a9ad1d844cd2395f8374d6193217a0
Julian Fajardo <julienf0410@gmail.com>
2022-01-31 17:36:55 -0300

    Example_Program added

---
+++ AutoGUI/Example_Program.cs
@@ -0,0 +1,9 @@
+﻿using AutoGUI;
+
+// Move the cursor at the position X=100 Y=100 and click on it
+Cursor.SetPos(100, 100);
+Cursor.LeftClick();
+
+
+// On any input field
+Keyboard.Set("Hola mundo!");
